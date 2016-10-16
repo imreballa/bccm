ff86dd81b23742641ea42361c4afc88861b0c331
Balla <imre.balla@workaholic.hu>
2016-10-16 09:25:17 +0200

    Model implementation

---
+++ codecool/mentor_candidates/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Mentor(models.Model):
+    first_name = models.CharField(max_length = 128, blank = False)
+    last_name = models.CharField(max_length = 128, blank = False)
+    email = models.EmailField(max_length = 128, blank = False, unique = True)
+    description = models.TextField
+    
+class Opinion(models.Model):
+    mentor = models.ForeignKey(Mentor)
+    name = models.CharField(max_length = 256, blank = False)
+    description = models.TextField(blank = False)
+
