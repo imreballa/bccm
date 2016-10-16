8cdc6f4b07678a718de5c9030f6def32ef99d805
Balla <imre.balla@workaholic.hu>
2016-10-16 09:51:03 +0200

    Add admin user Tweak mentor admin UI

--- codecool/mentor_candidates/models.py
+++ codecool/mentor_candidates/models.py
@@ -5,9 +5,11 @@ class Mentor(models.Model):
     last_name = models.CharField(max_length = 128, blank = False)
     email = models.EmailField(max_length = 128, blank = False, unique = True)
     description = models.TextField(blank = True, null = True)
-    
+    def __str__(self): return '{} {}'.format(self.last_name, self.first_name)
+        
 class Opinion(models.Model):
     mentor = models.ForeignKey(Mentor)
     name = models.CharField(max_length = 256, blank = False)
     description = models.TextField(blank = False)
+    def __str__(self): return '{} {}'.format(self.name, self.mentor)
 
